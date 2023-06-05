8a86017c223e77d7200eb215b17092105810075d
Martin Podloucký <podlouckymartin@gmail.com>
2023-06-05 21:40:36 +0200

    Update global exports from the package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { Visitor } from "./visitor.js";
 import { parseEffml } from "./parser.js";
 import { parseToAst } from "./ast.js";
 import { documentToString } from "./transform/to-string.js";
+import { documentToHtml } from "./transform/to-html.js";
 
-export { parseEffml, parseToAst, documentToString };
+export { parseEffml, parseToAst, documentToString, documentToHtml, Visitor };
